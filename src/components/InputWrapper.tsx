024fe472073aab2e9fd39c1171cf45eec62d2714
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-06-14 14:02:19 -0300

    Create reusable input wrapper

---
+++ src/components/InputWrapper.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import { Label } from './ui/label';
+
+const InputWrapper = ({
+  children,
+  label,
+  inputId,
+  error,
+}: {
+  children: ReactNode;
+  label: string;
+  inputId: string;
+  error?: string;
+}) => {
+  return (
+    <div>
+      <Label htmlFor={inputId}>{label}</Label>
+      {children}
+      {error && error.length > 0 ? (
+        <p className='text-sm px-1 text-red-500'>{error}</p>
+      ) : null}
+    </div>
+  );
+};
+
+export default InputWrapper;
