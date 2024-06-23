c297ac221e531f062a8ed97a98befde732f53e49
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-06-22 23:48:00 -0300

    Rebuild interviews form ui

--- src/app/interview/new/(components)/NewInterviewForm.tsx
+++ src/app/interview/new/(components)/NewInterviewForm.tsx
@@ -1,34 +1,29 @@
 'use client';
 
-import { Experience } from '@prisma/client';
-import { HiSparkles } from 'react-icons/hi2';
+import { HiMiniArrowLongLeft, HiSparkles } from 'react-icons/hi2';
 import { useForm } from 'react-hook-form';
 import { useMutation } from '@tanstack/react-query';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { LuLoader2 } from 'react-icons/lu';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
+import { motion } from 'framer-motion';
 
 import InputWrapper from '@/components/InputWrapper';
+import Radio from '@/components/ui/radio';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { InterviewSchema } from '@/validators';
 import { createInterview as createInterviewAction } from '@/actions/interview';
+import { useMultiStepForm } from '@/hooks/useMultistepForm';
 
 const NewInterviewForm = () => {
   const router = useRouter();
   const {
     register,
     handleSubmit,
-    setValue,
+    trigger,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(InterviewSchema),
@@ -43,83 +38,126 @@ const NewInterviewForm = () => {
     onError: () => toast.error('Failed to create interview'),
   });
 
-  const experienceLevels = Object.values(Experience);
+  const steps = [['jobRole'], ['jobExperience'], ['jobDescription']];
+
+  const { crrIndex, crrStep, nextStep, prevStep, isFirstStep, isLastStep } =
+    useMultiStepForm([
+      <motion.div
+        initial={{ x: '50%', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        key='role-step'
+      >
+        <p className='text-3xl font-medium tracking-tight mb-3 text-center'>
+          What&apos;s the job role?
+        </p>
+        <InputWrapper inputId='role' error={errors.jobRole?.message as string}>
+          <Input
+            id='role'
+            type='text'
+            placeholder='Role you are applying'
+            {...register('jobRole')}
+            className='h-12 lg:mt-4'
+          />
+        </InputWrapper>
+      </motion.div>,
+      <motion.div
+        initial={{ x: '50%', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        key='experience-step'
+      >
+        <p className='text-3xl font-medium tracking-tight mb-3 text-center'>
+          What&apos;s the required experience level?
+        </p>
+        <div className='text-center lg:pt-4 lg:text-start'>
+          <InputWrapper
+            inputId='experience'
+            error={errors.jobExperience?.message as string}
+          >
+            <Radio
+              options={[
+                { label: 'Intership', value: 'intership' },
+                { label: 'Junior', value: 'junior' },
+                { label: 'Srr', value: 'ssr' },
+                { label: 'Senior', value: 'senior' },
+                { label: 'Leader', value: 'lead' },
+              ]}
+              fieldName='jobExperience'
+              register={register}
+            />
+          </InputWrapper>
+        </div>
+      </motion.div>,
+      <motion.div
+        initial={{ x: '50%', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        key='description-step'
+      >
+        <p className='text-3xl font-medium tracking-tight mb-3 text-center'>
+          Describe what you need for this role
+        </p>
+        <InputWrapper
+          inputId='description'
+          error={errors.jobDescription?.message as string}
+        >
+          <Textarea
+            id='description'
+            placeholder='Job description and what you need to know'
+            rows={7}
+            className='resize-none lg:mt-4'
+            {...register('jobDescription')}
+          />
+        </InputWrapper>
+      </motion.div>,
+    ]);
 
   return (
     <form
-      onSubmit={handleSubmit(data => createInterview({ data }))}
+      onSubmit={
+        isLastStep
+          ? handleSubmit(data => createInterview({ data }))
+          : async e => {
+              e.preventDefault();
+
+              const fields = steps[crrIndex];
+              const isValid = await trigger(fields);
+
+              if (!isValid) return;
+
+              nextStep();
+            }
+      }
       className='flex flex-col gap-2'
     >
-      <InputWrapper
-        inputId='role'
-        label='Role'
-        error={errors.jobRole?.message as string}
+      {crrStep}
+      <motion.div
+        initial={{ x: '50%', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        className='mt-3 flex flex-col gap-2 md:flex-row-reverse md:justify-between'
       >
-        <Input
-          id='role'
-          type='text'
-          placeholder='Job role'
-          {...register('jobRole')}
-        />
-      </InputWrapper>
-      <InputWrapper
-        inputId='experience'
-        label='Experience level'
-        error={errors.jobExperience?.message as string}
-      >
-        <Select onValueChange={val => setValue('jobExperience', val)}>
-          <SelectTrigger
-            id='experience'
-            className='capitalize'
-          >
-            <SelectValue
-              {...register('jobExperience')}
-              placeholder='Select needed experience'
-              className='hover:bg-background-2'
-            />
-          </SelectTrigger>
-          <SelectContent id='experience'>
-            {experienceLevels.map(level => (
-              <SelectItem
-                value={level}
-                key={level}
-                className='capitalize'
-              >
-                {level}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </InputWrapper>
-      <InputWrapper
-        inputId='description'
-        label='Description'
-        error={errors.jobDescription?.message as string}
-      >
-        <Textarea
-          id='description'
-          placeholder='Job description and what you need to know'
-          rows={7}
-          className='resize-none'
-          {...register('jobDescription')}
-        />
-      </InputWrapper>
-      <div className='mt-1 md:flex md:items-center md:justify-end'>
-        <Button
-          disabled={isPending}
-          className='w-full md:w-auto'
-        >
+        <Button disabled={isPending} className='w-full md:w-auto' size='lg'>
           {isPending ? (
-            <LuLoader2
-              strokeWidth={2}
-              className='animate-spin size-4 mr-1.5'
-            />
-          ) : (
+            <LuLoader2 strokeWidth={2} className='animate-spin size-4 mr-1.5' />
+          ) : isLastStep ? (
             <HiSparkles className='size-4 mr-1.5' />
-          )}
-          Create with AI
+          ) : null}
+          {!isLastStep ? 'Next' : 'Create with AI'}
         </Button>
-      </div>
+        {!isFirstStep && (
+          <Button
+            onClick={e => prevStep(e)}
+            variant='ghost'
+            size='lg'
+            className='w-full md:w-auto'
+          >
+            <HiMiniArrowLongLeft className='size-4 mr-1.5' />
+            Go back
+          </Button>
+        )}
+      </motion.div>
     </form>
   );
 };
--- src/app/interview/new/page.tsx
+++ src/app/interview/new/page.tsx
@@ -2,29 +2,24 @@ import Link from 'next/link';
 import { HiArrowLeft } from 'react-icons/hi2';
 
 import NewInterviewForm from './(components)/NewInterviewForm';
-import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
 const NewInterviewPage = () => {
   return (
     <section className='h-full w-full flex flex-col items-center justify-center'>
       <div className='w-full max-w-[550px] px-6'>
-        <h1 className='text-3xl font-medium tracking-tight'>New Interview</h1>
-        <p className='tracking-tight mb-4 text-muted-foreground'>
-          Provide your job information and requirements to create the perfect
-          interview.
-        </p>
+        
         <NewInterviewForm />
       </div>
-      <Link
-        className={cn(
-          buttonVariants({ variant: 'ghost', size: 'sm' }),
-          'absolute top-2.5 left-2.5'
-        )}
-        href='/dashboard'
-      >
-        <HiArrowLeft className='size-4 mr-1.5' /> Go back
-      </Link>
+      <div className='flex items-center justify-between w-full absolute top-4 left-0 px-4 md:px-16 lg:px-32 xl:px-56'>
+        <Link
+          className={buttonVariants({ variant: 'ghost', size: 'sm' })}
+          href='/dashboard'
+        >
+          <HiArrowLeft className='size-4 mr-1.5' /> Go back
+        </Link>
+        <h2>MockAI</h2>
+      </div>
     </section>
   );
 };
