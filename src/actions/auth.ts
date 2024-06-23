513d6366754b7a7ef12a4a8c4b8930952e1dd953
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-06-22 23:49:03 -0300

    Implement new authentication action

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,4 +1,5 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { db } from '@/db';
 
 export const authAction = async () => {
   const { getUser } = getKindeServerSession();
@@ -7,4 +8,18 @@ export const authAction = async () => {
   if (!user) throw new Error('You need to log in');
 
   return user;
+};
+
+export const belongsToUser = async (userId: string, interviewId: string) => {
+  const interview = await db.interview.findUnique({
+    where: { id: interviewId },
+    select: { userId: true },
+  });
+
+  if (!interview) throw new Error('Interview not found');
+
+  if (interview?.userId !== userId)
+    throw new Error('This interview does not belong to you');
+
+  return true;
 };
