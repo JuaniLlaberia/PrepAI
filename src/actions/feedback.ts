0ba5b4d562504273ed3832b31caf785314459171
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-07-03 23:01:29 -0300

    Update feedback actions

--- src/actions/feedback.ts
+++ src/actions/feedback.ts
@@ -2,7 +2,7 @@
 
 import { db } from '@/db';
 import { model } from '@/gemini';
-import { authAction } from './auth';
+import { authAction, belongsToUser } from './auth';
 
 type GenerateFeedbackType = {
   userResponses: {
@@ -19,11 +19,14 @@ export const generateFeedback = async ({
   attemptId,
   interviewId,
 }: GenerateFeedbackType) => {
+  await authAction();
+
   const prompt = `Generate feedback on how the user responded to the questions. This is the json ${JSON.stringify(
     userResponses
   )} containing the questions and the user answers. Provide a feedback and a score (1 to 10, 1 being very poorly and 10 being excellent) for each question/answer pair. The feedback must be between 3-5 lines. Return it in this json schema format: {feedbacks: [{userResponse: userResponse.answer, feedback, score, questionId: userResponse.questionId, interviewId: ${interviewId}, interviewAttemptId: ${attemptId}}]}.
+
+  In addition return a speech analysis based on the users responses (analyse the vocabulary, the words used and it's repetition and return a feedback between 4-6 (create it as if you were talking to the user) lines in the same json under the name of speechAnalysis.
 `;
-  //With all the user responses (answers), generate a brief text (3-5 lines) analysis the speech and words used by the user (In order to help them improve their speech by using more suitable words, not repeat the same words all the time, etc.). Make sure you are talking to the user. Return this in a separate json with this schema: {speechAnalysis: analysis}.
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
@@ -38,10 +41,30 @@ export const generateFeedback = async ({
     interviewAttemptId: string;
   }[] = JSON.parse(jsonData).feedbacks;
 
-  await db.answer.createMany({ data: answers });
-  await db.interview.update({
-    where: { id: interviewId },
-    data: { taken: true },
+  await db.$transaction([
+    db.answer.createMany({ data: answers }),
+    db.interview.update({
+      where: { id: interviewId },
+      data: {
+        taken: true,
+      },
+    }),
+    db.interviewAttempt.update({
+      where: { id: attemptId },
+      data: {
+        speechAnalysis: JSON.parse(jsonData).speechAnalysis as string,
+      },
+    }),
+  ]);
+};
+
+export const getAttempts = async ({ interviewId }: { interviewId: string }) => {
+  const user = await authAction();
+  await belongsToUser(user.id, interviewId);
+
+  return await db.interviewAttempt.findMany({
+    where: { interviewId },
+    select: { id: true },
   });
 };
 
@@ -52,12 +75,25 @@ export const getAttemptFeedback = async ({
   interviewId: string;
   interviewAttemptId: string;
 }) => {
-  await authAction();
+  const user = await authAction();
+  await belongsToUser(user.id, interviewId);
 
-  const feedback = await db.answer.findMany({
+  const feedbackQuery = db.answer.findMany({
     where: { interviewId, interviewAttemptId },
-    include: { question: { select: { question: true } } },
+    include: {
+      question: { select: { question: true } },
+    },
   });
 
-  return feedback;
+  const analysisQuery = db.interviewAttempt.findUnique({
+    where: { id: interviewAttemptId },
+    select: { speechAnalysis: true },
+  });
+
+  const [feedback, analysis] = await Promise.all([
+    feedbackQuery,
+    analysisQuery,
+  ]);
+
+  return { feedback, analysis };
 };
