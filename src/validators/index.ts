8d8324b7320862ea9df6e85bc3481cbd0b97f8ff
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-07-04 23:05:58 -0300

    Add exams zod schema

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,4 +1,4 @@
-import { Experience } from '@prisma/client';
+import { Difficulty, Experience } from '@prisma/client';
 import { z } from 'zod';
 
 export const InterviewSchema = z.object({
@@ -13,4 +13,14 @@ export const InterviewSchema = z.object({
     .string()
     .min(20, { message: 'Must be at least 20 characters.' })
     .max(300, { message: 'Must have less than 300 characters.' }),
+});
+
+export const ExamSchema = z.object({
+  subject: z
+    .string()
+    .min(4, 'Must be at least 4 characters.')
+    .max(40, 'Must have less than 40 characters.'),
+  difficulty: z.nativeEnum(Difficulty, {
+    message: 'Please select a difficulty.',
+  }),
 });
