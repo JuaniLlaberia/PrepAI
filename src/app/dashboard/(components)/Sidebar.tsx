40d792305c0c117586d04962914b1bd83d90140b
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-07-14 00:36:51 -0300

    Update dashboard layout

---
+++ src/app/dashboard/(components)/Sidebar.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { NavigationLinksType } from '../layout';
+import { cn } from '@/lib/utils';
+
+const SidebarLinks = ({ links }: { links: NavigationLinksType[] }) => {
+  const pathname = usePathname();
+
+  return (
+    <ul className='flex flex-col gap-1'>
+      {links.map(link => (
+        <li key={link.id}>
+          <Link
+            href={link.link}
+            key={link.id}
+            className={cn(
+              'flex items-center gap-2 py-1.5 px-3 rounded-lg font-medium cursor-pointer text-muted-foreground hover:bg-muted/70 transition-colors',
+              pathname === link.link ? 'text-primary bg-muted/70' : null
+            )}
+          >
+            {link.icon}
+            <p className='text-sm'>{link.label}</p>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SidebarLinks;
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,36 @@
-import { ReactNode } from 'react';
+import { ReactElement, ReactNode } from 'react';
+import {
+  HiOutlineArrowUpCircle,
+  HiOutlineDocumentText,
+  HiOutlineSquares2X2,
+} from 'react-icons/hi2';
 
 import UserMenu from './(components)/UserMenu';
-import DashboardTabs from './(components)/Tabs';
+import SidebarLinks from './(components)/Sidebar';
+import { Button } from '@/components/ui/button';
+import DashboardTabs from './(components)/DashboardTabs';
+
+export type NavigationLinksType = {
+  id: string;
+  label: string;
+  link: string;
+  icon: ReactElement;
+};
+
+const navigationLinks: NavigationLinksType[] = [
+  {
+    id: 'interviews',
+    label: 'Interviews',
+    link: '/dashboard',
+    icon: <HiOutlineSquares2X2 strokeWidth={2} className='size-[1.1rem]' />,
+  },
+  {
+    id: 'mock-exams',
+    label: 'Mock exams',
+    link: '/dashboard/exams',
+    icon: <HiOutlineDocumentText strokeWidth={2} className='size-[1.1rem]' />,
+  },
+];
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
@@ -11,10 +40,33 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
           <h1>MockAI</h1>
           <UserMenu />
         </div>
-        <DashboardTabs />
+        <DashboardTabs links={navigationLinks} />
       </nav>
       <div className='lg:flex lg:flex-row'>
-        <section className='w-full pt-4 px-4 md:px-16 lg:px-32 xl:px-56 bg-background-2 dark:bg-background min-h-[calc(100vh-6.5rem-1px)] lg:min-h-screen'>
+        <aside className='p-3 w-[300px] border-r border-border hidden lg:flex flex-col'>
+          <div className='flex-1'>
+            <h1 className='text-center mb-6'>MockAI</h1>
+            <h2 className='px-1 mb-1 text-xs font-medium text-muted-foreground'>
+              Features
+            </h2>
+            <SidebarLinks links={navigationLinks} />
+          </div>
+          <div className='flex flex-col gap-4'>
+            <Button
+              size='sm'
+              variant='outline'
+              className='h-7 text-muted-foreground'
+            >
+              <HiOutlineArrowUpCircle
+                strokeWidth={1.5}
+                className='size-[1.1rem] mr-2'
+              />
+              Upgrade your plan
+            </Button>
+            <UserMenu />
+          </div>
+        </aside>
+        <section className='w-full pt-4 lg:pt-8 px-4 md:px-16 lg:px-20 2xl:px-56 bg-background-2 dark:bg-background min-h-[calc(100vh-6.5rem-1px)] lg:min-h-screen'>
           {children}
         </section>
       </div>
