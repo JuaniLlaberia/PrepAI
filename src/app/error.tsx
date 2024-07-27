c2ce1eb2b9fc336fb3ce04994438f8401968daa9
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-07-27 13:07:23 -0300

    Implement error pages

---
+++ src/app/error.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import Badge from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+
+const ErrorPage = ({ error }: { error: Error & { digest?: string } }) => {
+  const router = useRouter();
+  return (
+    <main className='flex flex-col py-2 px-4 md:px-16 lg:px-32 xl:px-56 h-screen'>
+      <header className='flex items-center py-3'>
+        <h1>MockAI</h1>
+      </header>
+      <section className='flex flex-col flex-1 items-center justify-center'>
+        <div className='flex flex-col gap-3 items-start'>
+          <Badge text='Error' color='red' />
+          <div className='mb-2'>
+            <h2 className='text-2xl font-medium'>
+              Oops! Something went wrong.
+            </h2>
+            <p className='text-muted-foreground'>
+              Sorry, something didn&apos;t go as expected. Try reloading or come
+              back later.
+            </p>
+          </div>
+          <Button onClick={() => router.refresh()}>Reload page</Button>
+        </div>
+      </section>
+      <footer className='px-4 pb-4'>
+        <p className='text-xs md:text-sm text-center text-muted-foreground'>
+          MockAI &copy; {new Date().getFullYear()} All rights reserved
+        </p>
+      </footer>
+    </main>
+  );
+};
+
+export default ErrorPage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+import Badge from '@/components/ui/badge';
+import GoBackBtn from '@/components/GoBackBtn';
+
+const NotFoundPage = () => {
+  return (
+    <main className='flex flex-col py-2 px-4 md:px-16 lg:px-32 xl:px-56 h-screen'>
+      <header className='flex items-center py-3'>
+        <h1>MockAI</h1>
+      </header>
+      <section className='flex flex-col flex-1 items-center justify-center'>
+        <div className='flex flex-col gap-3 items-start'>
+          <Badge text='404 page' color='gray' />
+          <div className='mb-2'>
+            <h2 className='text-2xl font-medium'>Oops! Page not found.</h2>
+            <p className='text-muted-foreground'>
+              Sorry, we could&apos;t find the page you where looking for.
+            </p>
+          </div>
+          <GoBackBtn />
+        </div>
+      </section>
+      <footer className='px-4 pb-4'>
+        <p className='text-xs md:text-sm text-center text-muted-foreground'>
+          MockAI &copy; {new Date().getFullYear()} All rights reserved
+        </p>
+      </footer>
+    </main>
+  );
+};
+
+export default NotFoundPage;
