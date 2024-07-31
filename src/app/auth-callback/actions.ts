f16b0bd4f4fc9c1e4aff4a5e7b1695298d7ea671
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-07-31 00:53:27 -0300

    Implement new actions architecture to users

--- src/app/auth-callback/actions.ts
+++ src/app/auth-callback/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import User from '@/db/models/User';
+import { createUser, findUserByKindeId } from '@/access-data/user';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 export const authenticateUser = async () => {
@@ -9,13 +9,15 @@ export const authenticateUser = async () => {
 
   if (!user?.email || !user.id) throw new Error('Failed to authenticate');
 
-  const userDB = await User.findOne({ kindeId: user.id });
+  const userDB = await findUserByKindeId(user.id);
 
   if (!userDB) {
-    await User.create({
-      kindeId: user.id,
-      email: user.email,
-      name: `${user.given_name} ${user.family_name}` ?? '',
+    await createUser({
+      user: {
+        kindeId: user.id,
+        email: user.email,
+        name: `${user.given_name} ${user.family_name}` ?? '',
+      },
     });
   }
 
