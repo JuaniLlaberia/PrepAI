8cdced7e62eebf8318667c97fa7c606f29bf3eef
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-08-08 12:59:26 -0300

    Update reusable ui components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,14 +9,16 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
+        default:
+          'bg-violet-500 text-white border-violet-500 border-[1px] shadow-[0_2px_0_rgba(109,40,217,1)] active:shadow-none active:translate-y-[2px] md:hover:opacity-85',
         destructive:
           'bg-destructive text-destructive-foreground border-[1px] border-[#ff6959] shadow-[0_2px_0_#ff6959] active:shadow-none active:translate-y-[2px] md:hover:bg-destructive/90',
         outline:
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-white border-slate-200 border-[1px] shadow-[0_2px_0_rgba(226,232,250,1)] active:shadow-none active:translate-y-[2px] md:hover:bg-slate-100 text-slate-500',
-        default:
-          'bg-violet-500 text-white border-violet-500 border-[1px] shadow-[0_2px_0_rgba(109,40,217,1)] active:shadow-none active:translate-y-[2px] md:hover:bg-violet-400',
+        'call-to-action':
+          'text-black bg-gray-100 dark:bg-white border-[1px] shadow-[0_2px_0] border-gray-200 shadow-gray-200 dark:border-gray-400 dark:shadow-gray-400 active:shadow-none active:translate-y-[2px] md:hover:bg-white transition-colors',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
