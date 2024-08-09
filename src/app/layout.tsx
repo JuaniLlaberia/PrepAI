a72b80b0070076ed149ead464410b433bbf19538
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-08-09 13:13:05 -0300

    Update default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <Toaster richColors />
         <ThemeProvider
           attribute='class'
-          defaultTheme='system'
+          defaultTheme='light'
           enableSystem
           disableTransitionOnChange
         >
