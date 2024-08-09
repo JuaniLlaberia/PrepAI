fa9c667b41a507d9383ffc1868884139925e7b99
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-08-09 16:19:25 -0300

    Try changing mongoose method

--- src/access-data/user.ts
+++ src/access-data/user.ts
@@ -2,7 +2,8 @@ import User, { IUserDocument } from '@/db/models/User';
 
 export const findUserByKindeId = async (kindeId: string) => {
   console.log('KINDE ID', kindeId);
-  return await User.findOne({ kindeId });
+  // return await User.findOne({ kindeId }).lean();
+  return await User.find({ kindeId });
 };
 
 export const createUser = async ({
